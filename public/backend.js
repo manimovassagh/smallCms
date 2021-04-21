ffb5c569cd30a8584c38652cabd992e2ec351151
Mani Movassagh <mani.mgh@gmail.com>
2021-04-21 17:36:33 +0200

    stock on backend

--- public/backend.js
+++ public/backend.js
@@ -1,10 +1,38 @@
+'use strict';
 
+//define variables
 const btn = document.querySelector('#btn')
 const input = document.querySelector('#input')
-console.log(input.value);
 
-document.addEventListener("DOMContentLoaded", evt => {
-btn.addEventListener('click',()=>{
-console.log(input.value);
-})
-});
+
+// define a post Methos function to post and fetch the post
+// fetch meaning is not *** get but here fetch send the data and act as post
+
+const postingData=()=>{
+    const newPost = new Request(
+        'save_data',
+        {
+            method:'post',
+            headers:{'content-type': 'application/json'},
+            body:JSON.stringify({
+                inhalt:input.value
+            })
+        }
+    )
+
+    fetch(newPost).then(
+        res => res.text()
+    ).then(
+        data=>console.log(data)
+    ).catch(
+        console.log
+    )
+}
+
+//define a function to fire after click and do the upper jobs
+
+const init =()=>{
+    btn.addEventListener('click',postingData)
+}
+
+init();
--- server.js
+++ server.js
@@ -10,7 +10,59 @@ server.use(express.static('public', {
 
 server.listen(portNumber, err => console.log(err || `Server is Active on Port ${portNumber}`));
 
+let paths = {
+    content: 'data/inhalt.json'
+}
 
+
+//function for read and write
+
+// FUNKTIONEN
+const loadFile = pfad => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(
+            pfad,
+            (err, data) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    data = data.toString();
+                    resolve(data);
+                }
+            }
+        )
+    })
+}
+const saveFile = (pfad, data) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(
+            pfad,
+            data,
+            err => {
+                if (err) {
+                    reject();
+                } else {
+                    resolve();
+                }
+            }
+        )
+    })
+}
+
+
+
+
+//Json usage 
+server.use(express.json());
+
+
+server.post('./save_data', (req, res) => {
+    saveFile(paths.content, JSON.stringify(req.body)).then(
+        () => res.send('ok')
+    ).catch(
+        () => res.send('unok')
+    )
+})
 
 
 
