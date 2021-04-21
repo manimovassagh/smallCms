8f02458b2e6c7e52e1dabd58feb5d8444c3a7aa4
Mani Movassagh <mani.mgh@gmail.com>
2021-04-21 16:12:12 +0200

    5

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const express = require('express');
 const server = express();
 const portNumber=80
 //define and activate the server
-server.use(express.static('public'));
+server.use(express.static('public', {
+    extensions: ['html']
+}));
 
 server.listen(portNumber, err => console.log(err || `Server is Active on Port ${portNumber}`));
 
